ebeb5708c4fb900b50e8929c49110f69ea016c50
justnnamdi01 <nwukwannamdi01@gmail.com>
2022-10-28 14:57:26 -0700

    g

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,6 @@ def fizzbuzz():
             print("FizzBuzz", end=' ')
         elif i % 3 == 0:
             print("Fizz", end=' ')
-        elif i % 5 == 0:                                                               print("Buzz", end=' ')                                                                       else:
-             print(i, end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        else:                                                                           print(i, end=' ')
