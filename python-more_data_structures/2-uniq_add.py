3bd65377ec4bab11e9578c7c531f5e9c3919c9f2
justnnamdi01 <nwukwannamdi01@gmail.com>
2022-11-04 14:13:03 -0700

    sd

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-     mem = []
-     for item in my_list:
-         if item not in mem:
-             mem.append(item)
-    sum = 0
-    for item in mem:
-        sum += item
-    return sum
+    uniq_list = list()
+    for item in my_list:
+        if item not in uniq_list:
+            uniq_list.append(item)
+    return sum(uniq_list)
