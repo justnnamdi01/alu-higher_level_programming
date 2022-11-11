186cdab1c3d04e411d0708e7c90af50a7bf61161
justnnamdi01 <nwukwannamdi01@gmail.com>
2022-11-11 13:12:33 -0800

    geekin

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"Create a square "
 
 
 class Square:
-    """Represent a square."""
+    '''
+    Create a square
+        Has a private Instance att: size
+    '''
 
     def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-            """
-            self.size = size
+        ''' init size '''
+        self.__size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        "returns the size att"
+        return self.__size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self, size):
+        '''asign the size to the size att'''
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        square_area = self.__size ** 2
+        return square_area
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"Create a square "
 
 
 class Square:
-    """Represent a square."""
+    '''
+    Create a square
+        Has a private Instance att: size
+    '''
 
-    def __init__(self, size):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-            """
-            self.size = size
+    def __init__(self, size=0):
+        ''' init size '''
+        self.__size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        "returns the size att"
+        return self.__size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def size(self, size):
+        '''asign the size to the size att'''
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        square_area = self.__size ** 2
+        return square_area
 
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-            if self.__size == 0:
-                print("")
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
