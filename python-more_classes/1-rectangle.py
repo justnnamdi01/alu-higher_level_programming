905f1792a5214b1efba19795197923d28314e2dd
justnnamdi01 <nwukwannamdi01@gmail.com>
2022-11-11 12:01:38 -0800

    fixed

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Define a Rectangle class."""
 
 
 class Rectangle:
-    """Represents a rectangle."""
+    """Represents a Rectangle."""
 
-     def __init__(self, width=0, height=0):
-         """Initialize a new Rectangle.
-         Args:
-         width (int): The width of the new rectangle.
-         height (int): The height of the new rectangle.
-         """
-         self.width = width
-         self.height = height
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int)= The width of the new rectangle.
+            height (int)= The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
