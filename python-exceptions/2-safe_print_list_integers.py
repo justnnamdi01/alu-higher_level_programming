7f42fcc65eb5c96f82ea5bd017ff5d12940413da
justnnamdi01 <nwukwannamdi01@gmail.com>
2022-11-11 08:23:25 -0800

    sd

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (TypeError, ValueError):
             pass
         count += 1
-        print()
-        return num
+    print()
+    return num
